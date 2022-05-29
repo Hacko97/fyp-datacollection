e8d255df88704e90338f2924366eacbab3a03bd7
Dines <42099788+Hacko97@users.noreply.github.com>
2022-05-29 18:14:40 +0530

    adding new structure

--- thamizhi.py
+++
@@ -1,9 +0,0 @@
-from thamizhilip import tamil
-tamil.downloadModels()
-
-#In order to use the dependency parser, you always need to load various models. 
-depModel=tamil.loadModels()
-
-#Then you can load them as shown below, 
-#when parsing a sentence. Need to feed one sentence at a time. 
-print(tamil.depTag("கண்ணன் அந்தப் புத்தகத்தைப் செய்தான்",depModel))
